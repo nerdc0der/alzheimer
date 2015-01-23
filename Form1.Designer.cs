215e21d17b5a6505cfb9823f2e6059185544d125
nerdc0der <web@nerdcoder.com>
2015-01-23 12:00:22 -0300

    Primer upload del codigo desarrollado

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AlzheimerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.btnStartProcess = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnFreeRAM = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblIteraciones = new System.Windows.Forms.Label();
+            this.lblRam = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblPercent = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartProcess
+            // 
+            this.btnStartProcess.Image = global::AlzheimerTest.Properties.Resources.gear_in;
+            this.btnStartProcess.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStartProcess.Location = new System.Drawing.Point(11, 76);
+            this.btnStartProcess.Name = "btnStartProcess";
+            this.btnStartProcess.Size = new System.Drawing.Size(129, 39);
+            this.btnStartProcess.TabIndex = 0;
+            this.btnStartProcess.Text = "Start";
+            this.btnStartProcess.UseVisualStyleBackColor = true;
+            this.btnStartProcess.Click += new System.EventHandler(this.btnStartProcess_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(306, 80);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(502, 35);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 121);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 17);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // btnFreeRAM
+            // 
+            this.btnFreeRAM.Image = global::AlzheimerTest.Properties.Resources.wrench;
+            this.btnFreeRAM.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFreeRAM.Location = new System.Drawing.Point(146, 76);
+            this.btnFreeRAM.Name = "btnFreeRAM";
+            this.btnFreeRAM.Size = new System.Drawing.Size(154, 39);
+            this.btnFreeRAM.TabIndex = 3;
+            this.btnFreeRAM.Text = "Liberar memoria";
+            this.btnFreeRAM.UseVisualStyleBackColor = true;
+            this.btnFreeRAM.Click += new System.EventHandler(this.btnFreeRAM_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(11, 25);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(502, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 100;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Iteraciones";
+            // 
+            // lblIteraciones
+            // 
+            this.lblIteraciones.AutoSize = true;
+            this.lblIteraciones.Location = new System.Drawing.Point(79, 10);
+            this.lblIteraciones.Name = "lblIteraciones";
+            this.lblIteraciones.Size = new System.Drawing.Size(0, 13);
+            this.lblIteraciones.TabIndex = 6;
+            // 
+            // lblRam
+            // 
+            this.lblRam.AutoSize = true;
+            this.lblRam.Location = new System.Drawing.Point(80, 154);
+            this.lblRam.Name = "lblRam";
+            this.lblRam.Size = new System.Drawing.Size(0, 13);
+            this.lblRam.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "RAM";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart2.BorderlineColor = System.Drawing.Color.DimGray;
+            this.chart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.AxisX.LineColor = System.Drawing.Color.DimGray;
+            chartArea2.AxisX.TitleForeColor = System.Drawing.Color.DimGray;
+            chartArea2.AxisX2.LineColor = System.Drawing.Color.DimGray;
+            chartArea2.AxisY.LineColor = System.Drawing.Color.DimGray;
+            chartArea2.AxisY.TitleForeColor = System.Drawing.Color.DimGray;
+            chartArea2.AxisY2.LineColor = System.Drawing.Color.DimGray;
+            chartArea2.BorderColor = System.Drawing.Color.Gray;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.ShadowOffset = 2;
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(17, 170);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.LabelForeColor = System.Drawing.Color.DimGray;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(497, 167);
+            this.chart2.TabIndex = 9;
+            title1.Name = "Title1";
+            title1.Text = "Uso de RAM (c/500ms)";
+            this.chart2.Titles.Add(title1);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(489, 121);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(0, 13);
+            this.lblPercent.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 348);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.lblRam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblIteraciones);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnFreeRAM);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStartProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "nerdcoder.com test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartProcess;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnFreeRAM;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblIteraciones;
+        private System.Windows.Forms.Label lblRam;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label lblPercent;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * nerdcoder.com 2014
+ * Aplicación de prueba para función de liberación de memoria alzheimer
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace AlzheimerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region nerdcoder.com: declaraciones
+        class arg
+        {
+            public int serie;
+            public Point punto;
+            public arg(int serie,Point punto)
+            {
+                this.serie = serie;
+                this.punto = punto;
+            }
+        }
+        int iteraciones;
+        int percent = 0; 
+        int serieJ = 0;
+        int x = 0;
+
+        /// <summary>
+        /// Consulta, calcula e informa el consumo de RAM de la actual aplicación
+        /// </summary>
+        public void RAM()
+        {
+            System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess();
+            long ramuse = ((proc.WorkingSet64 / 1024) / 1024);
+            lblRam.Text = ramuse.ToString() + "Mb";
+            chart2.Series[0].Points.Add(ramuse);
+            chart2.Update();
+        }
+
+        #endregion 
+
+        #region nerdcoder.com: alzheimer xD
+        [DllImport("kernel32.dll", EntryPoint = "SetProcessWorkingSetSize", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int SetProcessWorkingSetSize(IntPtr process, int minimumWorkingSetSize, int maximumWorkingSetSize);
+        public static void alzheimer()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            SetProcessWorkingSetSize(System.Diagnostics.Process.GetCurrentProcess().Handle, -1, -1);
+        }
+        #endregion
+
+        #region nerdcoder.com: BackGroundWorker
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (!backgroundWorker1.CancellationPending)
+            {
+                try
+                {
+                    for (int j = 1; j <= iteraciones; j++)
+                    {
+                        int percentage = ((j * 100) / iteraciones);
+                        for (int i = 1; i < 100; i++)
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            for (int x = 0; x < 1000000; x++)
+                                sb.AppendLine("*********************");
+                            /* reporte de progreso */
+                            backgroundWorker1.ReportProgress(percentage, new arg(j, new Point(j, i)));
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (!backgroundWorker1.CancellationPending)
+            {
+                arg p = (arg)e.UserState;
+
+                if (p.serie != serieJ)
+                {
+                    serieJ = p.serie;
+                    chart1.Series.Add(serieJ.ToString());
+                    chart1.Series[serieJ.ToString()].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+                }
+
+                chart1.Series[serieJ.ToString()].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(p.punto.X + (x++), p.punto.Y));
+                chart1.Update();
+                this.Text = "nerdcoder.com - test (" + p.punto.X + 1 + "," + p.punto.Y + ")";
+                if (percent < e.ProgressPercentage && e.ProgressPercentage < 101)
+                {
+                    progressBar1.Value = e.ProgressPercentage;
+                    percent = e.ProgressPercentage;
+                }
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!backgroundWorker1.CancellationPending)
+            {
+                progressBar1.Value = 100;
+                this.Text = "Done!";
+            }
+        }
+        #endregion
+
+        #region nerdcoder.com: ControlHandlers
+        private void btnStartProcess_Click(object sender, EventArgs e)
+        {
+            RAM();
+            progressBar1.Value = 0;
+            iteraciones = trackBar1.Value;
+            chart1.Series.Clear();
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void btnFreeRAM_Click(object sender, EventArgs e)
+        {
+            alzheimer();
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblIteraciones.Text = trackBar1.Value.ToString();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            RAM();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblIteraciones.Text = trackBar1.Value.ToString();
+        }
+        #endregion
+    }
+}
